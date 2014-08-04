02f23af4097ec9fc334beeb9333f178763d523cd
Tobias Burger <tobias_burger@hotmail.com>
2014-08-04 08:37:40 +0200

    Added some unit tests

--- MischkulturCalculator.Tests/Tests.fs
+++ MischkulturCalculator.Tests/Tests.fs
@@ -1,10 +1,31 @@
 ﻿module MischkulturCalculator.Tests
 
 open Xunit
+open FsUnit.Xunit
+open Calculator
+open Xunit.Extensions
 
-// place tests here
+let inline toTheoryData (seq: #seq<'a * 'b>): obj[][] =
+    seq |> Seq.map (fun (a, b) -> [| box a; box b |]) |> Seq.toArray
 
-[<Fact>]
-let ``test dummy`` () =
-    Assert.Equal(true, false)
+let wurzeldaten =
+    [ (Wurzel.Tief, Wurzel.Tief), false
+      (Wurzel.Flach, Wurzel.Flach), false
+      (Wurzel.Mittel, Wurzel.Mittel), false
+      (Wurzel.Tief, Wurzel.Flach), true
+      (Wurzel.Mittel, Wurzel.Flach), true
+      (Wurzel.Flach, Wurzel.Tief), true ]
+    |> toTheoryData
 
+[<Theory; PropertyData("wurzeldaten")>]
+let ``Wurzelunverträglichkeit`` ((w1, w2), expected) =
+    wurzelKompatibilität w1 w2 |> should equal expected
+
+let pflanzenfamiliendaten =
+    [ (Pflanzenfamilien.doldenblütler, Pflanzenfamilien.gänsefussgewächs), true
+      (Pflanzenfamilien.doldenblütler, Pflanzenfamilien.doldenblütler), false ]
+    |> toTheoryData
+
+[<Theory; PropertyData("pflanzenfamiliendaten")>]
+let ``Pflanzenfamilienverträglichkeit`` ((pf1: Pflanzenfamilie, pf2: Pflanzenfamilie), expected) =
+    pflanzenfamilieKompatibilität pf1 pf2 |> should equal expected
