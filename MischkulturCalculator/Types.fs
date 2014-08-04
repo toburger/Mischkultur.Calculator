7722927ee2443ca2c97a2e13fd1089588f6e8a90
Tobias Burger <tobias_burger@hotmail.com>
2014-08-04 08:59:20 +0200

    Added ToString overrides to types

--- MischkulturCalculator/Types.fs
+++ MischkulturCalculator/Types.fs
@@ -1,14 +1,40 @@
 ﻿namespace MischkulturCalculator
 
-type Wurzel = Tief | Mittel | Flach
-type Nährstoffverbrauch = Stark | Mittel | Schwach
-type Pflanzenfamilie = Pflanzenfamilie of string
+type Wurzel =
+    | Tief
+    | Mittel
+    | Flach
+    override self.ToString() = sprintf "%A" self
+
+type Nährstoffverbrauch =
+    | Stark
+    | Mittel
+    | Schwach
+    override self.ToString() = sprintf "%A" self
+
+type Pflanzenfamilie =
+    | Pflanzenfamilie of string
+    override self.ToString() =
+        let (Pflanzenfamilie(n)) = self
+        sprintf "%s" n
+
 type Pflanze =
     { Name : string
       Wurzel : Wurzel
       Nährstoffverbrauch : Nährstoffverbrauch
       Pflanzenfamilie : Pflanzenfamilie }
+    override self.ToString() = self.Name
 
 type Reihe = int
-type Vorgängerkultur = Vorgängerkultur of Pflanze * Reihe
-type Nachfolgekultur = Nachfolgekultur of Pflanze * Reihe
+
+type Vorgängerkultur =
+    | Vorgängerkultur of Pflanze * Reihe
+    override self.ToString() =
+        let (Vorgängerkultur(p, r)) = self
+        sprintf "%O, %i" p r
+
+type Nachfolgekultur =
+    | Nachfolgekultur of Pflanze * Reihe
+    override self.ToString() =
+        let (Nachfolgekultur(p, r)) = self
+        sprintf "%O, %i" p r
